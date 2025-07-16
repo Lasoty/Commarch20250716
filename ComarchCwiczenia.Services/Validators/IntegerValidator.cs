22d0b5a3d06280a50460ef72bd2f40ff1c7c0a24
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2025-07-16 14:09:24 +0200

    Testy integer

---
+++ ComarchCwiczenia.Services/Validators/IntegerValidator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComarchCwiczenia.Services.Validators;
+
+public class IntegerValidator
+{
+    public bool NumberIsPositive(int number)
+    {
+        return number > 0;
+    }
+}
---
+++ Tests/ComarchCwiczenia.Unit.Tests/Services/Validators/IntegerValidatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComarchCwiczenia.Services.Validators;
+
+namespace ComarchCwiczenia.Unit.Tests.Services.Validators;
+internal class IntegerValidatorTests
+{
+    [TestCase(1, true)]
+    [TestCase(0, false)]
+    [TestCase(-1, false)]
+    public void NumberIsPositive_BorderValues_ReturnsExpectedResult(int x, bool expected)
+    {
+        //Arrange
+        var cut = new IntegerValidator();
+
+        // Act
+        var actual = cut.NumberIsPositive(x);
+
+        // Assert
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void NumberIsPositive_AllPositiveRandomNumbers_ReturnsTrue([Random(1, int.MaxValue, 1)] int x)
+    {
+        //Arrange
+        var cut = new IntegerValidator();
+
+        // Act
+        var actual = cut.NumberIsPositive(x);
+
+        // Assert
+        Assert.That(actual, Is.True);
+    }
+}
