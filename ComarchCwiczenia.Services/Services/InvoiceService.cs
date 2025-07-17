5422833e8b9fbf609ec272b1bce069d4b9db1350
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2025-07-17 11:26:39 +0200

    Weryfikacja stringów za pomocą FluentAssertions

---
+++ ComarchCwiczenia.Services/Services/InvoiceService.cs
@@ -0,0 +1,11 @@
+﻿namespace ComarchCwiczenia.Services.Services;
+
+public class InvoiceService
+{
+    public string GetInvoiceNumber()
+    {
+        var datePart = DateTime.Now.ToString("yyyyMMdd");
+        var randomPart = new Random().Next(100, 999).ToString();
+        return $"INV-{datePart}-{randomPart}";
+    }
+}
---
+++ Tests/ComarchCwiczenia.Unit.Tests/Services/Services/InvoiceServiceFluentTests.cs
@@ -0,0 +1,47 @@
+﻿using ComarchCwiczenia.Services.Services;
+using FluentAssertions;
+
+namespace ComarchCwiczenia.Unit.Tests.Services.Services;
+
+public class InvoiceServiceFluentTests
+{
+    private InvoiceService _cut;
+
+    [SetUp]
+    public void Setup()
+    {
+        _cut = new InvoiceService();
+    }
+
+    [Test]
+    public void GetInvoiceNumber_ShouldStartWith_INV()
+    {
+        var actual = _cut.GetInvoiceNumber();
+        actual.Should().StartWith("INV-");
+    }
+
+    [Test]
+    public void GetInvoiceNumber_ShouldEndWith_NumberSuffix()
+    {
+        var actual = _cut.GetInvoiceNumber();
+        actual.Should().MatchRegex(@"INV-\d{8}-\d{3}$");
+    }
+
+    [Test]
+    public void GetInvoiceNumber_ShouldContain_CurrentDate()
+    {
+        var currentDate = DateTime.Now.ToString("yyyyMMdd");
+
+        var actual = _cut.GetInvoiceNumber();
+
+        actual.Should().Contain(currentDate);
+    }
+
+    [Test]
+    public void GetInvoiceNumber_ShouldMatch_ExpectedFormat()
+    {
+        var actual = _cut.GetInvoiceNumber();
+
+        actual.Should().Match("INV-????????-???").And.BeOfType<string>();
+    }
+}
